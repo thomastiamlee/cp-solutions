83685756e9b5b5d8b19b695d001cb2863cf465bc
Thomas <thomasjamestiamlee@gmail.com>
2023-06-06 02:08:24 +0800

    Added UVa 11172

---
+++ uva/11172.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        if (a < b)
+            printf("<\n");
+        else if (a > b)
+            printf(">\n");
+        else
+            printf("=\n");
+    }
+}
