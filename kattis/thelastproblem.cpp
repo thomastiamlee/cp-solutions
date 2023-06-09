d73fc466ffa33d35f612ec34f68659d56109b2b5
Thomas <thomasjamestiamlee@gmail.com>
2023-06-10 02:08:19 +0800

    Added kattis thelastproblem

---
+++ kattis/thelastproblem.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    getline(cin, name);
+
+    cout << "Thank you, " << name << ", and farewell!";
+}
